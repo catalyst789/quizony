d2773baa910cbf0a68b985cbf60bbb6671a34f03
Jalaj Kumar Gaur <raajk0338@gmail.com>
2021-06-27 12:06:02 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,21 @@
+// let categories = document.querySelector('#categories');
+// let ddc = document.querySelector('.drop-down-catgs');
+// let create = document.querySelector('#create');
+// let dropdowncreate = document.querySelector('.drop-down-create');
+
+// categories.addEventListener('mouseover', function(){
+//     ddc.style.opacity = 1;
+// });
+
+// categories.addEventListener('mouseout', function(){
+//     ddc.style.opacity = 0;
+// });
+
+// create.addEventListener('mouseover', function(){
+//     dropdowncreate.style.opacity = 1;
+// });
+
+// create.addEventListener('mouseout', function(){
+//     dropdowncreate.style.opacity = 0;
+// });
+
